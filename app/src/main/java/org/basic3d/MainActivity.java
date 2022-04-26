8327671ac330a71e7b3b00c105318907cee0ab0c
cornflower <ferdaus35-2959@diu.edu.bd>
2022-04-26 15:49:45 +0700

    Initialization

---
+++ app/src/main/java/org/basic3d/MainActivity.java
@@ -0,0 +1,13 @@
+package org.basic3d;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
